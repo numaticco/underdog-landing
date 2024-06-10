80455fd424e02ff211d890043e3ac28e8784ec2b
zb_m.fedotov <a.pervak@zebrains.team>
2024-06-10 17:07:42 +0300

    corrections on comments

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,12 +1,19 @@
 const aboutContainer = document.querySelector('.about')
 const peopleContainer = document.querySelector('.people')
 const brandContainer = document.querySelector('.brand')
+const activityContainer = document.querySelector('.activity')
 const peopleContainerTitle = document.querySelector('.people__title')
+const partnersContainer = document.querySelector('.activity__partners')
+const quoteContainer = document.querySelector('.activity__quote-wrapper')
 const brandContainerTitle = document.querySelector('.brand__title-logo')
+const activityDescriptionListImages = document.querySelector('.activity__description-list-images')
+const activityDescriptionList = document.querySelector('.activity__description-list')
+const activityArticlesDescriptionBlock = document.querySelector('.activity__articles-description-block')
 const mainScrollWrapper = document.querySelector('.main__scroll-wrapper')
 const aboutColorImages = document.querySelectorAll('.about__image-color')
 const activityDescriptionItems = document.querySelectorAll('.activity__description-item')
 const activityDescriptionImages = document.querySelectorAll('.activity__description-item-image')
+const activityArticlesItems = document.querySelectorAll('.activity__articles-item')
 
 // window. scrollTo(0, 0)
 
@@ -46,6 +53,39 @@ mainScrollWrapper.addEventListener('scroll', () => {
 
     if (peopleContainer.getBoundingClientRect().top < 300) {
         peopleContainerTitle.classList.add('aos-animate')
+    }
+
+    console.log(activityContainer.getBoundingClientRect().top);
+
+    if (activityContainer.getBoundingClientRect().top < 300) {
+        activityDescriptionListImages.classList.add('aos-animate')
+        activityDescriptionList.classList.add('aos-animate')
+    }
+
+    if (activityContainer.getBoundingClientRect().top < -20) {
+        activityArticlesDescriptionBlock.classList.add('aos-animate')
+    }
+
+    if (activityContainer.getBoundingClientRect().top < 100) {
+        mainScrollWrapper.style['scroll-snap-type'] = 'none'
+    }
+
+    if (activityContainer.getBoundingClientRect().top > 100) {
+        mainScrollWrapper.style['scroll-snap-type'] = 'y mandatory'
+    }
+
+    if (activityContainer.getBoundingClientRect().top < -600) {
+        activityArticlesItems.forEach(item => {
+            item.classList.add('aos-animate')
+        })
+    }
+
+    if (activityContainer.getBoundingClientRect().top < -800) {
+        partnersContainer.classList.add('aos-animate')
+    }
+
+    if (activityContainer.getBoundingClientRect().top < -1200) {
+        quoteContainer.classList.add('aos-animate')
     }
 })
 
